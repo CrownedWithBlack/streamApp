a130f59eb435120daa852d208a0efa922f0f7285
Fernando Zaragoza <fernandozaragoza@live.com>
2025-04-19 19:40:02 -0600

    saving changes before a major UI change

--- src/app/pages/catalogo/catalogo.component.ts
+++ src/app/pages/catalogo/catalogo.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoadDataService } from '../../services/json/load-data.service';
+import { NgForOf } from '@angular/common';
+import { pipe, map } from  'rxjs';
 
 @Component({
   selector: 'app-catalogo',
   standalone: true,
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './catalogo.component.html',
   styleUrl: './catalogo.component.scss'
 })
-export class CatalogoComponent {
+export class CatalogoComponent implements OnInit{
+  moviesPerSlide: any[][] = [];
+  
+  constructor(private loadDataService: LoadDataService) {}
 
+  ngOnInit(): void {
+    const amountOfMoviesPerSlide = 3;
+
+
+
+    this.loadDataService.getMovies().subscribe((movies) => {
+      this.moviesPerSlide = this.groupMoviesInSets(movies, amountOfMoviesPerSlide);
+      console.log('Movies grouped in sets of 3:', this.moviesPerSlide);
+    });
+
+    
+  }
+
+  private groupMoviesInSets(movies: any[], groupSize: number): any[][] {
+    const groupedMovies: any[][] = [];
+    for (let i = 0; i < movies.length; i += groupSize) {
+      groupedMovies.push(movies.slice(i, i + groupSize));
+    }
+    return groupedMovies;
+  }
 }
